62c5430ea9e0c64b0a81e2370778b23447417dbf
Omar Munoz <A01169881@itesm.mx>
2023-04-12 11:27:32 -0600

    Checkpoint

--- main.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-import pandas as pd
-import numpy as np
-import csv
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print('Hello world')
